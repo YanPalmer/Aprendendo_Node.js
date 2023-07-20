9977ba7a83ed422a0f4db70879c99b199002c51c
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-07-20 18:56:06 -0300

    Exercício 2 resolvido!

--- Exercicio_18_07/Ex_02.js
+++ Exercicio_18_07/Ex_02.js
@@ -1,2 +1,21 @@
 /* Exercício 2: Escreva uma função em JavaScript que receba dois números inteiros como parâmetros e retorne o maior número entre eles. 
 */
+
+const prompt = require("prompt-sync")();
+const entrada1 = prompt("Digite um número inteiro: ");
+const entrada2 = prompt("Digite outro número inteiro: ");
+
+if (entrada1 & entrada2) {
+    if (entrada1 > entrada2) {  // Verifica se o primeiro valor é maior que o segundo
+        console.log(`O maior número entre ${entrada1} e ${entrada2} é: ${entrada1}`);
+    }
+    else if (entrada2 > entrada1) { // Verifica se o segundo valor é maior que o primeiro
+        console.log(`O maior número entre ${entrada1} e ${entrada2} é: ${entrada2}`);
+    } else {    // Caso não haja números maiores eles provavelmente serão maiores
+        console.log("Os números devem ser iguais. Tente novamente...");
+    }
+} else {
+    prompt("Digite nos dois campos!");
+}
+
+// OK
--- Exercicio_18_07/Ex_03.js
+++ Exercicio_18_07/Ex_03.js
@@ -1,7 +1,11 @@
 /* Exercício 3: Escreva um programa em JavaScript que solicite ao usuário um número inteiro positivo e exiba na tela todos os números primos até esse número.
 */
 
+const prompt = require("prompt-sync")();
 const entrada = prompt("Digite um número inteiro positivo: "); // entrada: 10
-for (var i = 0; i < entrada.length; i++) {
 
+for (var i = 0; i < entrada.length; i++) {
+    if((entrada % 2 == 0) & (entrada % 3 == 0) & (entrada % 5 == 0)) {
+        console.log("Não é um número primo")
+    }
 }
