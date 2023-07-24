3027c0ef7619aa77d4690f824c0bc8816af13502
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-07-24 00:02:39 -0300

    Código totalmente único

--- Exercicio_18_07/Ex_03.js
+++ Exercicio_18_07/Ex_03.js
@@ -2,22 +2,23 @@
 */
 
 const prompt = require("prompt-sync")();
-const entrada = Number(prompt("Digite um número inteiro positivo: ")); // entrada: 11
 verificarEntrada();
 function verificarEntrada() {
-    if (entrada == 0 || entrada == 1) {
-        console.log("Obs: O valor digitado não pode ser 0 ou 1");
+  const entrada = Number(prompt("Digite um número inteiro positivo: ")); // entrada: 11
+    if (entrada == 0) {
+        console.log("Obs: O valor digitado não pode ser 0");
         console.log("Tente novamente...");
         return verificarEntrada();
+    } else if (entrada == 1) {
+      console.log(`O valor ${entrada} não é primo`)
     } else {
-        for (let i = 2; i < entrada; i++) {
-            if(i % 2 != 1 || i % 3 != 1 || i % 5 != 1) {
-                console.log(`O valor ${i} é primo`);
+        console.log("Aqui estão os números primos: ")
+        for (let i = 2; i <= entrada; i++) {
+            if((i % 1 == 0 & i % i == 0) & (i % 2 != 0 || i == 2) & (i % 3 != 0 || i == 3) & (i % 5 != 0 || i == 5) & (i % 7 != 0 || i == 7)) {
+              console.log(i)
             } else {
-                console.log(`O valor ${i} não é primo`);
             }
         }
-        console.log("Fím do código...")
     }
-
+  console.log("Fím do código...")
 }
